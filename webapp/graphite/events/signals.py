211e0827fc38f4b7a333e9f9152a7cfa797de4ce
Andrew Mezentzev <amezentzev@odesk.com>
2016-11-02 03:17:49 +0400

    Cleanup tagitem after event deletion

---
+++ webapp/graphite/events/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import signals
+from tagging.models import TaggedItem
+
+
+def taggeditem_delete(sender, **kwargs):
+    deleted = kwargs['instance']
+    try:
+        ob_id = int(deleted.pk)
+    except ValueError:
+        return
+    ctype = ContentType.objects.get_for_model(deleted)
+    item_tags = TaggedItem.objects.filter(content_type=ctype, object_id=ob_id,)
+    item_tags.delete()
+
+signals.post_delete.connect(taggeditem_delete)
